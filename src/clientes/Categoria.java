c747e87508b76fb6afc751ff15d890a32757cbff
PEDRO LUÍS SERRA FERNANDES <pedro_l_fernandes@estudante.sesisenai.org.br>
2025-10-23 19:57:08 -0300

    Categoria ENUM

--- src/clientes/Categoria.java
+++ src/clientes/Categoria.java
@@ -1,6 +1,18 @@
 package clientes;
 
 public enum Categoria {
+    BRONZE(0.0),
+    PRATA(0.05),
+    OURO(0.10),
+    PLATINA(0.15);
 
-    GESTOR, CLIENTE
+    private final double desconto;
+
+    Categoria(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
 }
