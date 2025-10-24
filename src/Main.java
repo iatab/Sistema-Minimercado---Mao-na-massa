13d385a963a7e08599c5fdb0ad8d2cdff92a838a
iata.b <iatamybilton@outlook.com>
2025-10-23 22:05:34 -0300

    enviado o main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import clientes.*;
 import produtos.Produto;
 import produtos.ProdutosService;
 import vendas.IVendaService;
@@ -12,10 +13,11 @@ public class Main {
     public static void main(String[] args) {
         ProdutosService produtosService = new ProdutosService();
         IVendaService vendaService = new VendaService(produtosService);
-
+        ClienteService clienteService = new ClienteService();
 
       popularProdutos(produtosService);
-     testarVendas(vendaService, produtosService);
+      popularClientes(clienteService);
+     testarVendas(vendaService, produtosService, clienteService);
       testarProdutos(produtosService);
 
 
@@ -40,9 +42,53 @@ public class Main {
     }
 
 
+    public static void popularClientes (ClienteService clienteService) {
+        ClientePessoaFisica cliente1 = new ClientePessoaFisica(
+                1,
+                "João Silva",
+                "123.456.789-00",
+                "99999-9999",
+                Categoria.OURO
+        );
+
+        ClientePessoaFisica cliente2 = new ClientePessoaFisica(
+                2,
+                "Maria Souza",
+                "987.654.321-00",
+                "98888-8888",
+                Categoria.OURO
+        );
+
+        // Criando clientes pessoa jurídica
+        ClientePessoaJuridica cliente3 = new ClientePessoaJuridica(
+                3,
+                "Tech Solutions LTDA",
+                "12.345.678/0001-90",
+                "97777-7777",
+                Categoria.PLATINA
+        );
+
+        ClientePessoaJuridica cliente4 = new ClientePessoaJuridica(
+                4,
+                "Comercial Silva ME",
+                "98.765.432/0001-10",
+                "96666-6666",
+                Categoria.BRONZE
+        );
+
+        clienteService.adicionarCliente(cliente1);
+        clienteService.adicionarCliente(cliente2);
+        clienteService.adicionarCliente(cliente3);
+        clienteService.adicionarCliente(cliente4);
 
 
-    public static void testarVendas(IVendaService vendaService, ProdutosService produtosService){
+
+    }
+
+
+
+
+    public static void testarVendas(IVendaService vendaService, ProdutosService produtosService, ClienteService clienteService) {
 
 
         // falta colocar o cliente e fazer fidelidade
@@ -52,11 +98,52 @@ public class Main {
         vendaService.iniciarVenda(v1);
         vendaService.adicionarItemDaVenda(1,1, 10);
         vendaService.adicionarItemDaVenda(2,1, 10);
+        vendaService.removerItemDaVenda(1,1);
 
         Venda v2 = new Venda(2);
         vendaService.iniciarVenda(v2);
         vendaService.adicionarItemDaVenda(2,2,10);
+        vendaService.aplicarDesconto(2);
+//        vendaService.listarVendas();
+        System.out.println("VENDAS COM CLIENTES ");
+        // verificar o remover item venda
+        // cliente pessoa fisica
+        Cliente cliente1 = clienteService.buscarPorId(1); // adiciono cliente previamente cadastrado
+        Venda v3 = new Venda(3,cliente1); // crio a venda
+        vendaService.iniciarVenda(v3);// inicio a venda
+        vendaService.adicionarItemDaVenda(1,3,2); // adiciono item a venda
+        vendaService.aplicarDesconto(3); // aplico o descono a venda
+//        vendaService.listarVendas(); // listo as vendas
+
+        Cliente cliente2 = clienteService.buscarPorId(2);
+        Venda v4 = new Venda(4,cliente2);
+        vendaService.iniciarVenda(v4);
+        vendaService.adicionarItemDaVenda(1,4,20);
+        vendaService.adicionarItemDaVenda(2,4,20);
+        vendaService.aplicarDesconto(4);
         vendaService.listarVendas();
+
+
+
+
+            // clientes cnpj
+        Cliente cliente3 = clienteService.buscarPorId(3);
+        Venda v5 = new Venda(5,cliente3);
+        vendaService.iniciarVenda(v5);
+        vendaService.adicionarItemDaVenda(1,5,20);
+        vendaService.adicionarItemDaVenda(3,5,20);
+        vendaService.aplicarDesconto(5);
+//        vendaService.listarVendas();
+
+
+        Cliente cliente4 = clienteService.buscarPorId(4);
+        Venda v6 = new Venda(6,cliente4);
+        vendaService.iniciarVenda(v6);
+        vendaService.adicionarItemDaVenda(3,6,20);
+        vendaService.adicionarItemDaVenda(2,6,20);
+        vendaService.aplicarDesconto(6);
+        vendaService.listarVendas();
+
 
 
     }
@@ -67,13 +154,14 @@ public class Main {
 
 
         System.out.println("POPULANDO DE PRODUTOS NO ESTOQUE");
-        Produto p1 = new Produto(1, "Arroz Branco Tipo 1", "7891234560011", 8.99, 6.50, 30);
-        Produto p2 = new Produto(2, "Feijão Carioca 1kg", "7891234560028", 6.49, 4.20, 25);
-        Produto p3 = new Produto(3, "Macarrão Espaguete 500g", "7891234560035", 4.29, 2.80, 40);
+        Produto p1 = new Produto(1, "Arroz Branco Tipo 1", "7891234560011", 8.99, 6.50, 300);
+        Produto p2 = new Produto(2, "Feijão Carioca 1kg", "7891234560028", 6.49, 4.20, 250);
+        Produto p3 = new Produto(3, "Macarrão Espaguete 500g", "7891234560035", 4.29, 2.80, 400);
 
         produtosService.cadastrarProduto(p1);
         produtosService.cadastrarProduto(p2);
         produtosService.cadastrarProduto(p3);
+
 
 
 
@@ -94,6 +182,7 @@ public class Main {
         produtosService.diminuirEstoqueProduto(2,35);
 
         produtosService.removerProduto(3);
+        produtosService.alterarPrecoProduto(1,20);
 
         produtosService.listarProdutos();
 
